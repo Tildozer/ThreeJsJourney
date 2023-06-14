573572000b687e5a305cbbb069f625402df8b2ee
Tildozer <actninswitch@gmail.com>
2023-06-14 08:39:55 -0600

    added green sock and made first animation

--- src/animation.js
+++ src/animation.js
@@ -5,27 +5,10 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-// same as doing the above
-mesh.position.set(0.7, -0.6, 1);
+
+
+mesh.position.set(0, 0, 0);
 scene.add(mesh);
-
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
-
-// Rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI / 4;
-mesh.rotation.y = Math.PI / 4;
-
-const axesHelper = new THREE.AxesHelper();
-
-scene.add(axesHelper);
 
 const [width, height] = [800, 600];
 const camera = new THREE.PerspectiveCamera(75, width / height);
@@ -38,4 +21,21 @@ camera.lookAt(mesh.position);
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(width, height);
-renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+// Animations
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+  // Update objects
+//   mesh.rotation.y = elapsedTime * Math.PI * 2;
+//   mesh.position.y = Math.sin(elapsedTime);
+//   mesh.position.x = Math.cos(elapsedTime);
+  camera.position.y = Math.sin(elapsedTime);
+  camera.position.x = Math.cos(elapsedTime);
+  camera.lookAt(mesh.position);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+
+tick();
