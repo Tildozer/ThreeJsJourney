4e6b4fbe28bf8edffe97e63a8e1f9b05ad3ca2a3
Anthony Thibodeaux <actninswitch@gmail.com>
2023-06-17 11:00:25 -0600

    done with light lesson, onto shadows.

--- src/scripts/lights.js
+++ src/scripts/lights.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 
@@ -18,13 +19,58 @@ const scene = new THREE.Scene();
  * Lights
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+directionalLight.position.set(1, 0.25, 0);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(1, -0.25, 1);
+scene.add(ambientLight, directionalLight, hemisphereLight, pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(
+  0x78ff00,
+  0.5,
+  10,
+  Math.PI * 0.1,
+  0.25,
+  1
+);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+spotLight.target.position.x = -0.75;
+
+scene.add(spotLight.target);
+
+// Helpers
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
+
+// scene.add(hemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  0.2
+);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+
+scene.add(
+  directionalLightHelper,
+  pointLightHelper,
+  spotLightHelper,
+  rectAreaLightHelper
+);
 
 /**
  * Objects
@@ -93,11 +139,60 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 /**
+ * Debug
+ */
+const ambientLightFolder = gui.addFolder("ambientLight");
+ambientLightFolder.close();
+
+ambientLightFolder.add(ambientLight, "intensity").min(0).max(1).step(0.01);
+ambientLightFolder.addColor(ambientLight, "color");
+
+const directionalLightFolder = gui.addFolder("directionalLight");
+directionalLightFolder.close();
+
+const directionalLightPosition = directionalLightFolder.addFolder("Position");
+directionalLightPosition.close();
+directionalLightPosition
+  .add(directionalLight.position, "x")
+  .min(-10)
+  .max(10)
+  .step(0.01);
+directionalLightPosition
+  .add(directionalLight.position, "y")
+  .min(-10)
+  .max(10)
+  .step(0.01);
+directionalLightPosition
+  .add(directionalLight.position, "z")
+  .min(-10)
+  .max(10)
+  .step(0.01);
+
+directionalLightFolder
+  .add(directionalLight, "intensity")
+  .min(0)
+  .max(1)
+  .step(0.01);
+directionalLightFolder.addColor(directionalLight, "color");
+
+const hemisphereLightFolder = gui.addFolder("hemisphereLight");
+hemisphereLightFolder.close();
+
+hemisphereLightFolder
+  .add(hemisphereLight, "intensity")
+  .min(0)
+  .max(1)
+  .step(0.01);
+hemisphereLightFolder.addColor(hemisphereLight, "color");
+hemisphereLightFolder.addColor(hemisphereLight, "groundColor");
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
