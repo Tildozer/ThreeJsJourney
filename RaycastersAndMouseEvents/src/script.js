c2c2c6dbfe98e41c9862ce27b661feb1d5e0a744
Anthony Thibodeaux <actninswitch@gmail.com>
2023-06-22 10:00:04 -0600

    finished raycaster and mouse events

--- RaycastersAndMouseEvents/src/script.js
+++ RaycastersAndMouseEvents/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 
@@ -15,6 +16,26 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+
+/**
+ * Models
+ */
+let model = null;
+
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load("/models/Duck/glTF-Binary/Duck.glb", (gltf) => {
+  gltf.scene.position.set(0, -1, 0);
+  scene.add(gltf.scene);
+  model = gltf.scene;
+});
+/**
+ * lights
+ */
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+directionalLight.position.set(1, 2, 3);
+scene.add(directionalLight);
 
 /**
  * Objects
@@ -37,6 +58,11 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2;
 
 scene.add(object1, object2, object3);
+/**
+ * Raycaster
+ */
+
+const raycaster = new THREE.Raycaster();
 
 /**
  * Sizes
@@ -58,6 +84,37 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+/**
+ * Mouse Events
+ */
+
+const mouse = new THREE.Vector2();
+mouse.x = -2;
+mouse.y = -2;
+
+window.addEventListener("mousemove", (ev) => {
+  mouse.x = (ev.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(ev.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener("click", (ev) => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log("object 1");
+        break;
+      case object2:
+        console.log("object 2");
+        break;
+      case object3:
+        console.log("object 3");
+        break;
+
+      default:
+        break;
+    }
+  }
 });
 
 /**
@@ -92,8 +149,53 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
+let currentIntersect = null;
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // aninmate Object
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 0.14) * 1.5;
+  object1.updateMatrixWorld();
+  object2.updateMatrixWorld();
+  object3.updateMatrixWorld();
+
+  // Cast Ray
+  // const raycasterOrgin = new THREE.Vector3(-3, 0, 0);
+  // const rayDirection = new THREE.Vector3(1, 0, 0);
+  // rayDirection.normalize();
+
+  // raycaster.set(raycasterOrgin, rayDirection);
+
+  raycaster.setFromCamera(mouse, camera);
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  objectsToTest.map((object) => object.material.color.set(0xff0000));
+  intersects.map((intersect) => intersect.object.material.color.set(0x0000ff));
+
+  if (intersects.length) {
+    if (currentIntersect === null) {
+      console.log("mouse enter");
+    }
+    currentIntersect = intersects[0];
+  } else {
+    if (currentIntersect) {
+      console.log("mouse leave");
+    }
+    currentIntersect = null;
+  }
+
+  if (model !== null) {
+    const modelIntersect = raycaster.intersectObject(model);
+    if (modelIntersect.length) {
+      model.scale.set(1.2, 1.2, 1.2);
+    } else {
+      model.scale.set(1, 1, 1);
+    }
+  }
 
   // Update controls
   controls.update();
