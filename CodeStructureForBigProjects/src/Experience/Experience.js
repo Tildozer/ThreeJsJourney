abc662e56720d4140280314240cd23c897444f14
Tildozer <actninswitch@gmail.com>
2023-07-05 13:18:12 -0600

    have my structure set up now to start building the scene

--- CodeStructureForBigProjects/src/Experience/Experience.js
+++ CodeStructureForBigProjects/src/Experience/Experience.js
@@ -1,6 +1,14 @@
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
 
 export default class Experience {
+  resize() {}
+
+  update() {
+    // console.log("update The Experience");
+  }
+
   constructor() {
     window.experience = this;
 
@@ -8,8 +16,13 @@ export default class Experience {
     const canvas = document.querySelector("canvas.webgl");
 
     this.canvas = canvas;
-    // console.log(this.canvas);
+    this.scene = new THREE.Scene();
 
     this.sizes = new Sizes(this.canvas);
+    this.time = new Time();
+
+    this.sizes.on("resize", () => this.resize());
+
+    this.time.on("tick", () => this.update());
   }
 }
--- CodeStructureForBigProjects/src/Experience/Utils/Sizes.js
+++ CodeStructureForBigProjects/src/Experience/Utils/Sizes.js
@@ -1,17 +1,21 @@
-export default class Sizes {
+import EventEmitter from "./EventEmitter";
+
+export default class Sizes extends EventEmitter {
   setParams() {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-
-    console.log(this);
   }
 
   constructor(canvas) {
+    super();
+
     this.setParams();
 
     window.addEventListener("resize", () => {
       this.setParams();
+
+      this.trigger("resize");
     });
   }
 }
