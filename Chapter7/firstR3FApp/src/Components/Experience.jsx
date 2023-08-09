b9cf04c36144d02a54e6ca5252619900f24867ef
Tildozer <actninswitch@gmail.com>
2023-08-08 22:25:27 -0600

    done with first R3F app

--- Chapter7/firstR3FApp/src/Components/Experience.jsx
+++ Chapter7/firstR3FApp/src/Components/Experience.jsx
@@ -12,16 +12,19 @@ const Experience = () => {
 
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta;
-
+    // const angle = state.clock.elapsedTime ;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0)
     // groupRef.current.rotation.y += delta;
     // console.log("tick");
   });
   return (
     <>
+      <orbitControls args={[camera, gl.domElement]} />
       <directionalLight position={[1, 2, 3]} />
       <ambientLight intensity={0.3} />
       <CustomObject />
-      <orbitControls args={[camera, gl.domElement]} />
       <group ref={groupRef}>
         <Ball />
         <Box refrence={cubeRef} />
--- Chapter7/firstR3FApp/src/index.jsx
+++ Chapter7/firstR3FApp/src/index.jsx
@@ -2,11 +2,34 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./Components";
+import {
+  ACESFilmicToneMapping,
+  CineonToneMapping,
+  SRGBColorSpace,
+} from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <Canvas>
+  <Canvas
+    // orthographic
+    // flat
+    gl={
+      {
+        // antialias: false //DEFAULT
+        // outputColorSpace: SRGBColorSpace, //DEFAULT
+        // toneMapping: ACESFilmicToneMapping //DEFAULT
+      }
+    }
+    camera={{
+      // fov: 45,
+      // zoom: 100,
+      near: 0.1,
+      far: 100,
+      position: [3, 2, 6],
+    }}
+    // dpr={[1, 2]}
+  >
     <Experience />
   </Canvas>,
 );
